d3e77d11fc9ba0874b96d0f7fa4a9b0a4e802a6b
Xhuljano Hamzallari <xhuljano.hamzallari@gmail.com>
2023-11-15 16:01:40 +0100

    final comit

--- esame-2023/src/main/java/it/euris/academi/esame2023/Contoller/TicketController.java
+++ esame-2023/src/main/java/it/euris/academi/esame2023/Contoller/TicketController.java
@@ -1,4 +1,11 @@
 package it.euris.academi.esame2023.Contoller;
 
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/tickets" )
 public class TicketController {
 }
